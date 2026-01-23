3e54bde9df4995212fb40cdecf1bb13553277b09
tm1812tkg <tkn1812.p128@outlook.jp>
2026-01-23 20:40:27 +0900

    kadai_015アップロード

---
+++ kadai_015/Car_Chapter15.java
@@ -0,0 +1,31 @@
+package kadai_015;
+
+public class Car_Chapter15 {
+
+	//フィールド（内部データ）
+	private int gear = 1;
+	private int speed = 10;
+
+	public void changeGear(int afterGear) {
+		
+		System.out.println("ギア" + this.gear + "から" + afterGear + "に切り替えました");
+			
+		this.gear = afterGear;
+
+		this.speed = switch (afterGear) {
+			case 1 -> 10;
+			case 2 -> 20;
+			case 3 -> 30;
+			case 4 -> 40;
+			case 5 -> 50;
+			default -> 10;
+		};
+
+		
+	}
+
+	public void run() {
+
+		System.out.println("速度は時速" + this.speed + "kmです");
+	}
+}
