9ba6683316c75284ffeb5f7b2d59c46bc30c81c4
tm1812tkg <tkn1812.p128@outlook.jp>
2026-01-28 23:06:33 +0900

    ２回目の修正

--- kadai_021/DictionaryExec_Chapter21.java
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -8,15 +8,7 @@ public class DictionaryExec_Chapter21 {
 
 		String[] words = { "apple", "banana", "grape", "orange" };
 
-		for (String word : words) {
-			String result = dic.fruit.get(word);
-
-			if (result != null) {
-				System.out.println(word + "の意味は" + result);
-			} else {
-				System.out.println(word + "は辞書に存在しません");
-			}
-		}
+		dic.searchWords(words);
 
 	}
 
