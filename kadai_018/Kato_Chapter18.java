8c0166ae813fe027f48c6d3dec51f9b1edcf4f66
tm1812tkg <tkn1812.p128@outlook.jp>
2026-01-31 16:59:22 +0900

    修正１回目

--- kadai_018/Kato_Chapter18.java
+++ kadai_018/Kato_Chapter18.java
@@ -2,14 +2,11 @@ package kadai_018;
 
 abstract public class Kato_Chapter18 {
 
-	String familyName;
+	String familyName = "加藤";
 	String givenName;
-	String address;
+	String address = "住所は東京都中野区〇×です";
 
-	Kato_Chapter18() {
-		this.familyName = "加藤";
-		this.address = "住所は東京都中野区〇×です";
-	}
+
 
 	public void commonIntroduce() {
 		System.out.println("名前は" + familyName + givenName + "です");
